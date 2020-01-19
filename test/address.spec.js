26381fabcef2ba45fcc5148903f03395d83628a2
listenaddress <tbobrien612@gmail.com>
2020-01-19 10:21:27 -0800

    Init repo

---
+++ test/address.spec.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+const { expect } = require('chai')
+
+describe('address', () => {
+  describe('decode', () => {
+    it('should decode ID address', async () => {
+      expect(true).to.be.eql(true)
+    })
+  })
+})
