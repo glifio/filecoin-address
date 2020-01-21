1aabb187ca1b4c269741fc8069c1445186c06e4d
Thomas O'Brien <tbobrien612@gmail.com>
2020-01-20 16:57:57 -0800

    Add Address class and tests (#2)
    * WIP
    
    * Add NewFromString tests for all protocols

--- src/index.js
+++ src/index.js
@@ -1,29 +1,69 @@
-/* eslint-disable radix */
+/* eslint-disable radix,prefer-const */
 const varint = require('varint')
 const { blake2b } = require('blakejs')
 const base32Function = require('./base32')
+// const { ID, SECP256K1, Actor, BLS } = require('./constants')
 
 const base32 = base32Function('abcdefghijklmnopqrstuvwxyz234567')
 
-const bigintToArray = v => {
+let newAddress
+let decode
+let bigintToArray
+let getChecksum
+let validateChecksum
+let NewFromString
+// let addressHash
+
+class Address {
+  constructor({ str }) {
+    if (!str) throw new Error('Missing str in address')
+    this.str = str
+  }
+
+  // newIDAddress = id => {
+  //   return newAddress(ID, varint.encode(id))
+  // }
+
+  // newSecp256k1Address = pubkey => {
+  //   return newAddress(SECP256K1, addressHash(pubkey))
+  // }
+
+  // newActorAddress = data => {
+  //   return newAddress(Actor, addressHash(data))
+  // }
+
+  // newBLSAddress = pubkey => {
+  //   return newAddress(BLS, pubkey)
+  // }
+}
+
+bigintToArray = v => {
   // eslint-disable-next-line no-undef
   let tmp = BigInt(v).toString(16)
-
-  // Not sure why it is not padding and buffer does not like it
   if (tmp.length % 2 === 1) tmp = `0${tmp}`
   return Buffer.from(tmp, 'hex')
 }
 
-const getChecksum = ingest => {
+getChecksum = ingest => {
   return blake2b(ingest, null, 4)
 }
 
-const validateChecksum = (ingest, expect) => {
+validateChecksum = (ingest, expect) => {
   const digest = getChecksum(ingest)
   return Buffer.compare(Buffer.from(digest), expect)
 }
 
-const decodeAddress = address => {
+newAddress = (protocol, payload) => {
+  // TODO: add string checks as seen in Lotus
+  const protocolByte = new Buffer.alloc(1)
+  protocolByte[0] = protocol
+
+  return new Address({
+    str: Buffer.concat([protocolByte, payload])
+  })
+}
+
+decode = address => {
   // TODO: add string checks as seen in Lotus
   const protocol = address.slice(1, 2)
   const protocolByte = new Buffer.alloc(1)
@@ -31,10 +71,8 @@ const decodeAddress = address => {
   const raw = address.substring(2, address.length)
 
   if (protocol === '0') {
-    return Buffer.concat([
-      protocolByte,
-      Buffer.from(varint.encode(parseInt(raw)))
-    ])
+    // TODO: Check for valid raw string
+    return newAddress(protocol, Buffer.from(varint.encode(parseInt(raw))))
   }
 
   const payloadChecksum = new Buffer.from(base32.decode(raw))
@@ -45,11 +83,22 @@ const decodeAddress = address => {
     throw Error("Checksums don't match")
   }
 
-  return Buffer.concat([protocolByte, payload])
+  return newAddress(protocol, payload)
 }
 
+NewFromString = address => {
+  return decode(address)
+}
+
+// addressHash = ingest => {
+//   return blake2b(ingest, null, 20)
+// }
+
 module.exports = {
+  Address,
   validateChecksum,
   bigintToArray,
-  decodeAddress
+  newAddress,
+  decode,
+  NewFromString
 }
--- test/address.spec.js
+++ test/address.spec.js
@@ -1,10 +1,67 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
+// const borc = require('borc')
+const { NewFromString } = require('../src/index')
+const {
+  IDAddresses,
+  secp256k1Addresses,
+  BLSAddresses,
+  actorAddresses
+} = require('./constants')
+
+function typedArraysAreEqual(a, b) {
+  if (a.byteLength !== b.byteLength) return false
+  return a.every((val, i) => val === b[i])
+}
 
 describe('address', () => {
-  describe('decode', () => {
-    it('should decode ID address', async () => {
-      expect(true).to.be.eql(true)
+  describe('NewFromString', () => {
+    it('should create new ID addresses', async () => {
+      IDAddresses.forEach(item => {
+        const address = NewFromString(item.string)
+        expect(
+          typedArraysAreEqual(
+            Uint8Array.from(address.str),
+            item.decodedByteArray
+          )
+        ).to.eql(true)
+      })
+    })
+
+    it('should create new secp256k1 addresses', async () => {
+      secp256k1Addresses.forEach(item => {
+        const address = NewFromString(item.string)
+        expect(
+          typedArraysAreEqual(
+            Uint8Array.from(address.str),
+            item.decodedByteArray
+          )
+        ).to.eql(true)
+      })
+    })
+
+    it('should create new BLS addresses', async () => {
+      BLSAddresses.forEach(item => {
+        const address = NewFromString(item.string)
+        expect(
+          typedArraysAreEqual(
+            Uint8Array.from(address.str),
+            item.decodedByteArray
+          )
+        ).to.eql(true)
+      })
+    })
+
+    it('should create new Actor addresses', async () => {
+      actorAddresses.forEach(item => {
+        const address = NewFromString(item.string)
+        expect(
+          typedArraysAreEqual(
+            Uint8Array.from(address.str),
+            item.decodedByteArray
+          )
+        ).to.eql(true)
+      })
     })
   })
 })
